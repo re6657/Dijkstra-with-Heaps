f9a35ff2206569e2c15dbf9c994d5fbd578e0bfe
Hzeristo <126323580+Hzeristo@users.noreply.github.com>
2024-03-28 11:06:18 +0800

    graph api
    图定义，方法完成

--- header/Dijkstra.h
+++ header/Dijkstra.h
@@ -5,19 +5,31 @@
 
 using namespace std;
 
-struct Edge{
-    string dest;
+class Edge{
+public:
+    int dest;
     int weight;
+    Edge(int& dest, int& weight):dest(dest), weight(weight){};
+    ~Edge(){};
 };
 
 class Graph{
 private:
     int V;
     int E;
-    unordered_map<string, vector<Edge>> adjList;
+    unordered_map<int, vector<Edge>> adjList;
 public:
-    Graph();
-    ~Graph();
+    Graph():V(0), E(0), adjList({}){};
+    ~Graph(){};
+    bool addVertex(int& name);
+    bool addEdge(int& name, Edge& newedge);
+    bool addEdge(int& name, int& dest, int& weight);
+    inline int getV() const;
+    inline int getE() const;
+    int getWeight(int& src, int& dest);
+    bool alterWeight(int& src, int& dest, int& weight);
+    vector<int>& getNeighbors(int& src);
+    Graph& buildGraphFromtxt(const string& filePath);
 };
 
 #endif
--- src/Dijkstra.cpp
+++ src/Dijkstra.cpp
@@ -2,3 +2,122 @@
 #include "Dijkstra.h"
 
 using namespace std;
+
+bool Graph::addVertex(int& name)
+{
+    if(adjList.find(name) != adjList.end())
+    {
+        throw "Insertion Failed: found element";
+        return false;
+    }
+    adjList[name] = vector<Edge>();
+    V++;
+    return true;
+}
+
+bool Graph::addEdge(int& name, Edge& newedge)
+{
+    if(newedge.dest == 0 || newedge.weight == 0)
+    {
+        throw "Inserted empty edge, operation aborted";
+        return true;
+    }
+    if(adjList.find(name) == adjList.end())
+        adjList[name] = vector<Edge>();
+    adjList[name].push_back(newedge);
+    V++;
+    E++;
+    return true;
+}
+
+bool Graph::addEdge(int& name, int& dest, int& weight)
+{
+    if(dest == 0 || weight == 0)
+    {
+        throw "Inserted empty edge, operation aborted";
+        return true;
+    }
+    if(adjList.find(name) == adjList.end())
+        adjList[name] = vector<Edge>();
+    adjList[name].push_back(Edge(dest, weight));
+    V++;
+    E++;
+    return true;
+}
+
+inline int Graph::getV() const
+{
+    return V;
+}
+
+inline int Graph::getE() const
+{
+    return E;
+}
+
+int Graph::getWeight(int& src, int& dest)
+{
+    if(adjList.find(src) == adjList.end())
+    {
+        throw "Source not found";
+        return 0;
+    }
+    for (Edge& edge : adjList[src])
+    {
+        if (edge.dest == dest)
+            return edge.weight;
+    }
+}
+
+bool Graph::alterWeight(int& src, int& dest, int& weight)
+{
+    if(adjList.find(src) == adjList.end())
+    {
+        throw "Source not found";
+        return false;
+    }
+    for (Edge& edge : adjList[src])
+    {
+        if (edge.dest == dest)
+        {
+            edge.weight = weight;
+            return true;
+        }
+    }
+}
+
+vector<int>& Graph::getNeighbors(int& src)
+{
+    vector<int> neighbors;
+    auto it = adjList.find(src);
+    if(it != adjList.end())
+        for(Edge& edge : it->second)
+            neighbors.push_back(edge.dest);
+    else
+    {
+        throw "Source not found";
+        static vector<int> emptyList;
+        return emptyList;
+    }
+    return neighbors;
+}
+
+Graph& Graph::buildGraphFromtxt(const string& filePath)
+{
+    ifstream file(filePath);
+    string line;
+    if(!file.is_open())
+    {
+        throw "Unable to open .txt file, check your directory";
+        static Graph emptyGraph;
+        return emptyGraph;
+    }
+    while(getline(file, line))
+    {
+        istringstream iss(line);
+        int src, dest, weight;
+        iss >> src >> dest >> weight;
+        addEdge(src, dest, weight);
+    }
+    return *this;
+}
