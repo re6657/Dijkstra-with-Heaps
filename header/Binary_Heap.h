3ba9dd65e0ddf673d7a6a447aa3d1dd58dc86151
re6657 <166217808+re6657@users.noreply.github.com>
2024-04-07 18:13:41 +0800

    Update Binary_Heap.h

--- header/Binary_Heap.h
+++ header/Binary_Heap.h
@@ -1,17 +1,99 @@
 #ifndef __BIH_H
 #define __BIH_H
-
 #include "default.h"
-
-class Binary_Heap
-{
-private:
-    
-public:
-    Binary_Heap(/* args */);
-    ~Binary_Heap();
-};
-
-
+using namespace std;  
+class Binary 
+{  
+	private:  
+	vector<pair<int, int> > node;  
+	int size = 0;  
+	
+	void swap(int i, int j)//交换
+	{  
+		pair<int, int> tmp = node[i];  
+		node[i] = node[j];  
+		node[j] = tmp;  
+	}  
+	
+	void up(int i)//上滤
+	{
+		while(i>0)
+		{
+			int dad=(i-1)/2;
+			if(node[dad].first>node[i].first)
+			{
+				swap(dad,i);
+				i=dad;
+			}
+			else
+				break;
+		}
+	}
+	
+	void down(int i)//下滤
+	{
+		int left;
+		int right;
+		while(2*i+2<=size)	
+		{
+			left=2*i+1;
+			right=2*i+2;
+			if(node[left].first>node[right].first)
+				left=right;
+			if(node[left].first<node[i].first)
+			{	
+				swap(left,i);
+				i=left;
+			}
+			else
+				break;
+		}	
+	}		
+	public:  
+	void insert(int j,int i)
+	{  
+	pair<int, int> p=make_pair(j,i); 
+			node.push_back(p);
+		size++;   
+		if(size>0)
+		up(size-1);   
+	}  
+	
+	void update(int j,int i)
+	{  
+		pair<int, int> p=make_pair(i,j);
+		node.push_back(p);   
+		size++;   
+		if(size>0)
+			up(size-1);   
+		
+	} 
+	
+	pair<int, int> removeMin()
+	{  
+		pair<int, int> tmp=node[0];  
+		node[0]=node.back();  
+		node.pop_back();  
+		size--;  
+		down(0);  
+		return tmp;  
+	}  
+	
+	//pair<int, int> minimum()
+	int minimum()
+	{  
+		return node[0].second;  
+	}  
+	
+	int Size() 
+	{  
+		return size;  
+	}  
+	
+	bool isEmpty()  
+	{  
+		return size == 0;  
+	}  
+};  
 
 #endif
